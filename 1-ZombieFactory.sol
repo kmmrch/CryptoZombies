06f3b2499574c10247b403aa994347e24ab2e0e8
marianakemmerich <123078429+marianakemmerich@users.noreply.github.com>
2023-03-20 16:21:25 -0300

    Modify createZombie function, declare an event
    "createZombie" was a public function by default, meaning that the contract was vulnerable to attacks and anyone could call said function, so it was modified to be a private function and now only the owner of the contract can execute the code.

--- 1-ZombieFactory.sol
+++ 1-ZombieFactory.sol
@@ -1,7 +1,10 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
-// this will be permanently stored in the blockchain
+
+event NewZombie(uint zombieId, string name, uint dna); // let the front-end know every time a new zombie is created
+
+    // this will be permanently stored in the blockchain
     uint dnaDigits = 16; // zombie's dna is determinated by a 16 digit number
     uint dnaModulus = 10 ** dnaDigits; // make sure the dna has ONLY 16 characters
     
@@ -12,7 +15,18 @@ contract ZombieFactory {
     
     Zombie[] public zombies; // stores an army of zombies
     
-    function createZombie(string memory _name, uint _dna) public {
-    zombies.push(Zombie(_name, _dna)); // adds the new zombie to the zombies array
+    function _createZombie(string memory _name, uint _dna) private { // private function names start with an underscore (_)
+    uint id = zombies.push(Zombie(_name, _dna)) - 1; // adds the new zombie to the zombies array
+        emit NewZombie(id, _name, _dna);
+    }
+    
+    function _generateRandomDna(string memory _str) private view returns (uint) { //views some f the contract's variabes but don't modify them
+        uint rand = uint(keccak256(abi.encodePacked(_str))); // generate a pseudo-random hexadecimal
+        return rand % dnaModulus;
+    }
+    
+    function createRandomZombie(string memory _name) public { // takes the zombie's name and use it to create a zombie with random dna
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 }
