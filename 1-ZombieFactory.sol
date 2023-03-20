ef838db3e75c63bc9c2a2e5e77a11e06ec2628ca
marianakemmerich <123078429+marianakemmerich@users.noreply.github.com>
2023-03-20 15:49:01 -0300

    Create a public struct, declare a function
    Created a struct with the zombies's properties, declared a function that creates a new zombie and stores it's name and dna

--- 1-ZombieFactory.sol
+++ 1-ZombieFactory.sol
@@ -4,4 +4,15 @@ contract ZombieFactory {
 // this will be permanently stored in the blockchain
     uint dnaDigits = 16; // zombie's dna is determinated by a 16 digit number
     uint dnaModulus = 10 ** dnaDigits; // make sure the dna has ONLY 16 characters
+    
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+    
+    Zombie[] public zombies; // stores an army of zombies
+    
+    function createZombie(string memory _name, uint _dna) public {
+    zombies.push(Zombie(_name, _dna)); // adds the new zombie to the zombies array
+    }
 }
