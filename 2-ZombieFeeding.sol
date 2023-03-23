31bb2dad1edc36758cc98b6565bbf884604459d0
marianakemmerich <123078429+marianakemmerich@users.noreply.github.com>
2023-03-23 17:09:26 -0300

    Implement a cooldown timer
    Changed the visibility of "feedAndMultiply" from public to internal to make the contract more secure

--- 2-ZombieFeeding.sol
+++ 2-ZombieFeeding.sol
@@ -24,16 +24,26 @@ KittyInterface kittyContract; // changed this line to just declare a variable
 function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
   }
+  
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
 
-function feedAndMultiply(uint _zombieId, uint _targetDna, string memoy _species) public {
+  function _isReady(Zombie storage _zombie) internal view returns (bool) { // tell us if enough time has passed since the last time the zombie fed
+      return (_zombie.readyTime <= now);
+  }
+
+function feedAndMultiply(uint _zombieId, uint _targetDna, string memoy _species) internal {
     require(msg.sender == zombieToOwner[_zombieId]); // verify if msg.sender is the zombie's owner
     Zombie storage myZombie = zombies[_zombieId]; // get the zombie's dna
+    require(_isReady(myZombie)); // the user can only execute this function if the zombie's cooldowntime is over
     _targetDna = _targetDna % dnaModulus; // make sure the dna isn't longer than 16 digits
     uint newDna = (myZombie.dna + _targetDna) / 2; // generate a new dna
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
       newDna = newDna - newDna % 100 + 99; // replace the last two digits of the dna with "99"
     }
     _createZombie("NoName", newDna);
+    _triggerCooldown(myZombie); // make feeding triggers the zombie's cooldown time
   }
 
 function feedOnKitty(uint _zombieId, uint _kittyId) public { // interact with CryptoKitties' contract
