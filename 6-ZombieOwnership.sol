6761a3ce53f9ee86df32fe1b35c082aa5fc18364
marianakemmerich <123078429+marianakemmerich@users.noreply.github.com>
2023-03-28 09:27:15 -0300

    Transfer logic

--- 6-ZombieOwnership.sol
+++ 6-ZombieOwnership.sol
@@ -12,6 +12,13 @@ function balanceOf(address _owner) external view returns (uint256) {
   function ownerOf(uint256 _tokenId) external view returns (address) {
     return zombieToOwner[_tokenId]; // return the owner of "_tokenId"
   }
+  
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to]++; // keep track of how many zombies an owner has
+    ownerZombieCount[_from]--; // keep track of who owns what
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
+  }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
 
